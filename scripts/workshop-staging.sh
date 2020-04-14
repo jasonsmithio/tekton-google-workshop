76d6b45205dcb91f5b80dd356eb5f27036465a0f
Jason (Jay) Smith <jaysmith@google.com>
2020-04-14 14:53:27 -0700

    Fixed Workshop script

--- scripts/workshop-staging.sh
+++ scripts/workshop-staging.sh
@@ -5,31 +5,37 @@
 
 export REGION='us-central1'
 export CLUSTER_NAME='gitlab-cluster'
-export EMAIL=''
+
 
 # execute GitLab commands commands
 # Using https://gitlab.com/gitlab-org/charts/gitlab/-/tree/master/scripts
 
 ./gitlab/gke_bootstrap_script.sh up
 
+sleep 45
+
 gcloud compute addresses create ${CLUSTER_NAME}-external-ip --region $REGION --project $PROJECT
 
 export EXTERNAL_IP=$(gcloud compute addresses describe ${CLUSTER_NAME}-external-ip --region $REGION --project $PROJECT --format='value(address)')
+echo 'your EXTERNAL IP is'$EXTERNAL_IP
 
 export DOMAIN=$EXTERNAL_IP'.xip.io'
+echo 'your DOMAIN is'$DOMAIN
+
 
 helm repo add gitlab https://charts.gitlab.io/
 helm repo update
 helm upgrade --install gitlab gitlab/gitlab \
-#  --timeout 600 \
-  --set global.hosts.domain=$DOMAIN \
-  --set global.hosts.externalIP=$EXTERNAL_IP \
-  --set certmanager-issuer.email=$EMAIL
+  --set global.hosts.domain=${DOMAIN} \
+  --set certmanager-issuer.email=${EMAIL} \
+  --set global.hosts.externalIP=${EXTERNAL_IP}
+  
 
 #export PASSWORD=$(kubectl get secret <name>-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 --decode ; echo)
+sleep 60
 
 export PASSWORD=$(kubectl get secret gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 --decode ; echo)
 
-echo 'your password is ' $PASSWORD
-echo 'your username is ' $EMAIL
-echo 'Please visit https://'$DOMAIN 'in your browser'
+echo 'your password is: ' $PASSWORD
+echo 'your username is: root'
+echo 'Please visit https://gitlab.'$DOMAIN 'in your browser'
