09d8e930a1e9137dad4e1ee26b364da4ed01e538
Jason (Jay) Smith <jaysmith@google.com>
2020-04-15 16:49:46 -0700

    GitLab on GKE working

--- scripts/workshop-staging.sh
+++ scripts/workshop-staging.sh
@@ -4,13 +4,23 @@
 ## https://docs.gitlab.com/charts/installation/deployment.html
 
 export REGION='us-central1'
+export ZONE='us-central1-f'
 export CLUSTER_NAME='gitlab-cluster'
 
 
 # execute GitLab commands commands
 # Using https://gitlab.com/gitlab-org/charts/gitlab/-/tree/master/scripts
 
-./gitlab/gke_bootstrap_script.sh up
+#./gitlab/gke_bootstrap_script.sh up
+
+gcloud container clusters create gitlab-cluster \
+    --zone $ZONE \
+    --cluster-version latest --machine-type n1-standard-4 \
+    --scopes cloud-platform \
+    --num-nodes 3\
+    --enable-ip-alias \
+    --project $PROJECT
+
 
 sleep 45
 
@@ -30,8 +40,6 @@ helm upgrade --install gitlab gitlab/gitlab \
   --set certmanager-issuer.email=${EMAIL} \
   --set global.hosts.externalIP=${EXTERNAL_IP}
   
-
-#export PASSWORD=$(kubectl get secret <name>-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 --decode ; echo)
 sleep 60
 
 export PASSWORD=$(kubectl get secret gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 --decode ; echo)
@@ -39,3 +47,20 @@ export PASSWORD=$(kubectl get secret gitlab-gitlab-initial-root-password -ojsonp
 echo 'your password is: ' $PASSWORD
 echo 'your username is: root'
 echo 'Please visit https://gitlab.'$DOMAIN 'in your browser'
+
+# Connect to cluster
+gcloud container clusters get-credentials gitlab-cluster --zone us-central1-b --project $PROJECT
+
+
+
+kubectl create clusterrolebinding cluster-admin-binding \
+--clusterrole=cluster-admin \
+--user=$(gcloud config get-value core/account)
+
+
+
+# Install Tekton Pipelines
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
+
+# Install Tekton Triggers
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
