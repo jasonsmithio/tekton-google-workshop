00c63649d601a2fe664501723faf318aeca233b4
Jay Smith <jaysmith@google.com>
2021-02-24 11:01:07 -0800

    gitlab sync

--- scripts/tekton-staging.sh
+++ scripts/tekton-staging.sh
@@ -6,7 +6,7 @@ set +x; echo "Connect to cluster..."
 set -x;
 gcloud container clusters get-credentials gitlab-cluster --zone ${ZONE} --project ${PROJECT_ID}
 set +x; echo
-  
+
 # Install Tekton Pipelines
 set +x; echo "Install Tekton Pipelines..."
 set -x
@@ -40,18 +40,18 @@ if ! [ -x "$(command -v tkn)" ]; then
     if [[ "$OSTYPE"  == "linux-gnu" ]]; then
         set -x;
         curl -LO https://github.com/tektoncd/cli/releases/download/v0.16.0/tkn_0.16.0_Linux_x86_64.tar.gz
-        sudo tar xvzf tkn_0.13.1_Linux_x86_64.tar.gz -C /usr/local/bin/ tkn
+        sudo tar xvzf tkn_0.16.0_Linux_x86_64.tar.gz -C /usr/local/bin/ tkn
         set +x;
 
 
     elif [[ "$OSTYPE" == "darwin"* ]]; then
         set -x;
         curl -LO https://github.com/tektoncd/cli/releases/download/v0.16.0/tkn_0.16.0_Darwin_x86_64.tar.gz
-        sudo tar xvzf tkn_0.13.1_Darwin_x86_64.tar.gz -C /usr/local/bin tkn
+        sudo tar xvzf tkn_0.16.0_Darwin_x86_64.tar.gz -C /usr/local/bin tkn
         set +x;
     else
         echo "unknown OS"
     fi
-else 
+else
     echo "TKN is already installed. Let's move on"
 fi
